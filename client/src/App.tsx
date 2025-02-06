57ed2cac06ceb7b251291d939c5d9e35ea0ad803
Kbenjamin92 <kipplbenjamin@gmail.com>
2025-02-05 21:16:41 -0500

    created the authcontext file and protected route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,20 @@
 import './App.css'
-import { Grid, GridItem, Box } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Signup } from './components/auth/Signup';
+import { Login } from './components/auth/Login';
+import { Main } from './components/main/Main';
 
 const App = () => {
   
   return (
     <>
-      <Grid 
-        templateColumns='repeat(4, 1fr)'
-        templateRows='repeat(3, 1fr)'
-        gap={1}
-        >
-        <GridItem colSpan={4} rowSpan={1}>
-          <Box background="dodgerblue" color='white' h='90px'>
-            Nav
-          </Box>
-        </GridItem>
-        <GridItem colSpan={3} rowSpan={2}>
-          <Box background="crimson" color='white' h='405px'>
-            Main
-          </Box>
-        </GridItem>
-        <GridItem colSpan={1} rowSpan={3}>
-          <Box background="gold" color='white' h='405px'>
-            Aside
-          </Box>
-        </GridItem>
-        <GridItem colSpan={4} rowSpan={1}>
-          <Box background="dodgerblue" color='white' h='100px'>
-            Footer
-          </Box>
-        </GridItem>
-      </Grid>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element ={<Main />} />
+      </Routes>
+    </Router>
     </>
   )
 }
---
+++ client/src/components/main/Main.tsx
@@ -0,0 +1,42 @@
+import { Grid, GridItem, Box } from '@chakra-ui/react';
+import { AuthProvider } from '@/context/AuthProvider';
+
+export const Main = () => {
+  return (
+    <>
+    <AuthProvider>
+        <Grid 
+          templateColumns={{
+            base: '1fr',
+            sm: 'repeat(2, 1fr)',
+            lg: 'repeat(4, 1fr)'
+          }}
+            templateRows='repeat(4)'
+            gap={2}
+            w='100vw'
+          >
+          <GridItem colSpan={4}>
+            <Box background="dodgerblue" color='white' h='85px'>
+                Nav
+            </Box>
+          </GridItem>
+          <GridItem colSpan={3} rowSpan={3}>
+            <Box background="gray" color='white' h='100vh'>
+                Main
+            </Box>
+          </GridItem>
+          <GridItem colSpan={1} rowSpan={3} >
+            <Box background="gold" color='white' h='100vh'>
+                Aside
+            </Box>
+          </GridItem>
+          <GridItem colSpan={4}>
+            <Box background="dodgerblue" color='white' h='10vh'>
+                Footer
+            </Box>
+          </GridItem>
+        </Grid>
+      </AuthProvider>
+    </>
+  )
+}
